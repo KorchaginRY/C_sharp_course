294c15b46594c20f7888a8a007f9e821d9b3050a
Roman <rom25k@mail.ru>
2022-06-26 18:53:03 +1000

    квадрат числа

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, all people!");
+﻿Console.Write("Введите 1е число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите 2е число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 * num1 == num2)
+{
+    Console.WriteLine("Число 1 является квадратом 2 числа ");
+}
+else
+{
+    Console.WriteLine("Число 1  НЕ является квадратом 2 числа ");
+}
