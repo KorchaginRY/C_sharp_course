e3bb7d7090b0e0c37c8ff3e8dd6ff028cddad3e6
Roman <rom25k@mail.ru>
2022-07-03 23:34:11 +1000

    HW_3_1-3_3

---
+++ 3_Lesson/HW_3_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Kub(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.Write($"{Math.Pow(count, 3)} ");
+        count++;
+    }
+
+}
+
+Kub(3);
+Kub(5);
+Console.WriteLine();
