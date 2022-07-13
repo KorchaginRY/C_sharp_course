ba4fddbf660594c04b971b87033a14f7af1b46e7
Roman <rom25k@mail.ru>
2022-07-13 14:16:59 +1000

    5_2

---
+++ 5_Lesson/5_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Программа замены элементов массива на противоположные.
+
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-10, 11);
+    }
+    return arr;
+}
+
+void ChangeNum(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+}
+
+int[] arr_1 = MassNums(12);
+Print(arr_1);
+ChangeNum(arr_1);
+Print(arr_1);
