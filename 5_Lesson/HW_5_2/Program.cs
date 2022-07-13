0687c8ab39312890850e0b80779dbb1a356b8947
Roman <rom25k@mail.ru>
2022-07-13 16:27:33 +1000

    HW_5_2

---
+++ 5_Lesson/HW_5_2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+//  Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-10, 20);
+    }
+    return arr;
+}
+
+void OddSum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i+=2)
+    {
+        sum += arr[i];
+    }
+    Console.WriteLine($"Сумма нечетных элементов = {sum}");
+}
+
+int[] arr_1 = MassNums(10);
+Print(arr_1);
+OddSum(arr_1);
