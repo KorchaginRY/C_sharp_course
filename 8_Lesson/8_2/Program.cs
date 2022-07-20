e9d8df13c515a62471d498e30eec288d5075b0b8
Roman <rom25k@mail.ru>
2022-07-20 11:10:11 +1000

    8_3

---
+++ 8_Lesson/8_2/Program.cs
@@ -0,0 +1,56 @@
+﻿// 2. Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это невозможно,
+// программа должна вывести сообщение для пользователя.
+
+void PrintMas(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] FillMas(int row, int column)
+{
+    int[,] arr = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+string ModifyArray(int[,] array)
+{
+    int row = array.GetLength(0);
+    int col = array.GetLength(1);
+
+    if (row != col) { return "No"; }
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            (array[i, j], array[j, i]) = (array[j, i], array[i, j]);
+        }
+
+    }
+    return "Yes";
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+int[,] arr_1 = FillMas(row, column);
+PrintMas(arr_1);
+Console.WriteLine(ModifyArray(arr_1));
+PrintMas(arr_1);
---
+++ 8_Lesson/8_3/Program.cs
@@ -0,0 +1,57 @@
+﻿// 3. Составить частотный словарь элементов двумерного массива.
+// Частотный словарь содержит информацию о том, сколько раз
+// встречается элемент входных данных. Значения элементов массива 0..9
+
+void PrintMas(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] FillMas(int row, int column)
+{
+    int[,] arr = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+int[] Vocabulary(int[,] arr)
+{
+    int[] array = new int[10];
+    foreach (int item in arr) array[item]++;
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        Console.Write($"{i} - {arr[i]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = FillMas(row, column);
+PrintMas(arr_1);
+int[] array_1 = Vocabulary(arr_1);
+PrintArray(array_1);
